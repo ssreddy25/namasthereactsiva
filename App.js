31d4f24fe3ca6cf3044a701a00a7d67a1ae6c0b0
ssreddy25 <sivasankarreddyuppaluru@gmail.com>
2023-08-23 18:54:28 +0530

    secon epside commits

--- App.js
+++ App.js
@@ -1,31 +1,7 @@
-/**
- * <div id="parent">
- *     <div id="child">
- *       <h1>hello world</h1>
- *       <h2>wlcome react corse</h2>
- *  </div>
- * <div id="child2">
- *       <h1>hello world</h1>
- *       <h2>wlcome react corse</h2>
- *  </div>
- * </div>
- * 
- * 
- * 
- * 
- * 
- */
-       const heading=React.createElement("div",{id:"parent"},[
-       React.createElement("div" ,{id:"child"},[
-       React.createElement("h1",{},"hello world"),
-       React.createElement("h2",{},"wlcome to react")]),
-       React.createElement("div" ,{id:"child2"},[
-        React.createElement("h1",{},"hello world"),
-        React.createElement("h2",{},"wlcome to react")])
-       
-       ]);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-/**const heading =React.createElement("h1",{id:"heading"},"welcom to react course ");*/
-   console.log(heading);
-          const root1=ReactDOM.createRoot(document.getElementById("root"))
-          root1.render(heading);
+       const heading=React.createElement("div",{id:"parent"},"i am siva sankar reddy")
+      
+          const root=ReactDOM.createRoot(document.getElementById("root"));
+          root.render(heading);
